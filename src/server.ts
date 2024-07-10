616f2a49e3bc638cd105adad940dad98400b2426
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-07-10 11:36:41 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import productsRoutes from './routes/productsRoutes';
+
 import dotenv from "dotenv";
 dotenv.config();
+
+
+
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use('/api/products', productsRoutes);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Clothes Shopping API');
+  res.send('Shopping API');
 });
 
 app.listen(port, () => {
