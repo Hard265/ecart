57f1669940e3c20ca9f45d103dd1926e7ebd2549
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-07-12 19:01:10 +0000

    sync

--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -1,1 +1,23 @@
-//TODO create categoriesRoutes.ts routes
+import { Router } from "express";
+import { authenticateToken } from "../middlewares/jwtMiddleware";
+import * as categoriesController from "../controllers/categoriesController";
+
+const router: Router = Router();
+
+router.get(
+    "/",
+    authenticateToken,
+    categoriesController.getAllCategories
+);
+router.post(
+    "/",
+    authenticateToken,
+    categoriesController.createCategory
+);
+router.delete(
+    "/:id",
+    authenticateToken,
+    categoriesController.deleteCategory
+);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
-import express, { Express, Request, Response } from "express";
+import express, {
+    Express,
+    Request,
+    Response,
+} from "express";
 import multer from "multer";
 import productsRoutes from "./routes/productsRoutes";
 import authentionsRoutes from "./routes/authentionsRoutes";
@@ -6,6 +10,7 @@ import usersRoutes from "./routes/usersRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import reviewsRoutes from "./routes/reviewsRoutes";
 import ordersRoutes from "./routes/ordersRoutes";
+import categoriesRoutes from "./routes/categoriesRoutes";
 import path from "path";
 import dotenv from "dotenv";
 import sequelize from "./sequelize";
@@ -16,14 +21,17 @@ sequelize.sync();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 export const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/");
-    },
-    filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname));
-    },
-  }),
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, "uploads/");
+        },
+        filename: (req, file, cb) => {
+            cb(
+                null,
+                Date.now() + path.extname(file.originalname)
+            );
+        },
+    }),
 });
 
 app.use(express.json());
@@ -34,11 +42,12 @@ app.use("/api/auth", authentionsRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", ordersRoutes);
+app.use("/api/categories", categoriesRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Shopping API");
+    res.send("Shopping API");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
