d68299b9cf103b9b428c6cfab1857d73c8162e45
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-07-13 08:51:50 +0200

    Update productsRoutes.ts
    Add the put and delete privileges

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,13 +1,14 @@
 import express, { Router } from "express";
 import * as productsController from "../controllers/productsController";
 import { authenticateToken } from "../middlewares/jwtMiddleware";
+import { verifyProductOwnership } from "../middleware/authorizationMiddleware";
 
 const router: Router = express.Router();
 
 router.get("/", authenticateToken, productsController.getAllProducts);
 router.get("/:id", authenticateToken, productsController.getProductsById);
 router.post("/", authenticateToken, productsController.createProducts);
-router.put("/:id", authenticateToken, productsController.updateProducts);
-router.delete("/:id", authenticateToken, productsController.deleteProducts);
+router.put("/:id", authenticateToken, verifyProductOwnership, productsController.updateProducts);
+router.delete("/:id", authenticateToken, verifyProductOwnership, productsController.deleteProducts);
 
 export default router;
