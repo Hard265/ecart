22a63dae7fe08b0518a94d0eaa23ac9eff53c696
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-07-15 01:17:34 +0000

    Commit

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import { authenticateToken } from "../middlewares/jwtMiddleware";
 import * as ordersController from "../controllers/ordersController";
+import {verifyOrderOwnership} from "../middlewares/authorizationMiddleware";
 
 const router: Router = express.Router();
 
 router.get("/", authenticateToken, ordersController.getAllOrders);
 router.post("/", authenticateToken, ordersController.createOrder);
 router.get("/:id", authenticateToken, ordersController.getOrderById);
-router.delete("/:id", authenticateToken, ordersController.deleteOrder);
+router.delete("/:id", authenticateToken, verifyOrderOwnership, ordersController.deleteOrder);
 
 export default router;
