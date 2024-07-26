b6334b6806643bb19b587c19a17abda56ded025e
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-07-26 04:00:57 +0200

    Update jwtMiddleware.ts

--- src/middlewares/jwtMiddleware.ts
+++ src/middlewares/jwtMiddleware.ts
@@ -3,13 +3,16 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/User";
 import { AuthenticatedRequest } from "../@types";
 
+interface JWTPayload extends jwt.JwtPayload {
+  userId: string;
+}
+
 export const authenticateToken = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
@@ -21,22 +24,14 @@ export const authenticateToken = async (
   }
 
   try {
-    const { userId, exp } = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    ) as jwt.JwtPayload & { userId: string };
-
-    if (!userId) {
-      return res.status(401).json({ error: "Invalid token" });
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as JWTPayload
+    if (!decoded.userId || (decoded.exp && Date.now() >= decoded.exp * 1000)) {
+      return res.status(401).json({ error: "Invalid or expired token" });
     }
 
-    if (exp && Date.now() >= exp * 1000) {
-      return res.status(401).json({ error: "Token has expired" });
-    }
-
-    const user = await User.findOne({ where: { id: userId } });
+    const user = await User.findOne({ where: { id: decoded.userId } });
     if (!user) {
-      return res.status(406).json({ error: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
 
     (req as AuthenticatedRequest).user = user;
