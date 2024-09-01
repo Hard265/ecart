8e39839c3da760e92724b92d2211c16f77dfa883
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-09-01 03:25:50 +0000

    move to postgressql database

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "@sequelize/core";
 import { SqliteDialect } from "@sequelize/sqlite3";
+import { PostgresDialect } from '@sequelize/postgres';
 import { Product } from "./models/Product";
 import { User } from "./models/User";
 import { Cart, CartItem } from "./models/Cart";
@@ -8,19 +9,18 @@ import { Category } from "./models/Category";
 import { Review } from "./models/Review";
 
 const sequelize = new Sequelize({
-  dialect: SqliteDialect,
-  storage: "sequelize.sqlite",
+  dialect: PostgresDialect,
+  database: "ecart_7gdv",
+  user: "hard265",
+  password: "rJvAFCHX1QWLxuHnOl0rmLo86l2OXSpX",
+  url: "postgresql://hard265:rJvAFCHX1QWLxuHnOl0rmLo86l2OXSpX@dpg-cr9tk7ij1k6c73blh8qg-a.oregon-postgres.render.com/ecart_7gdv",
+  clientMinMessages: "notice",
+ // port: 5432,
   models: [
     Product,
     User,
     Cart/* CartItem, Review, Order, OrderItem, Category*/,
   ],
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 });
 
 export default sequelize;
